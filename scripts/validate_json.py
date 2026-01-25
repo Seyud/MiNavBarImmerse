32e61ac00e215bc1343c8fc8f87f01692f3989b6
Ianzb <93322252@qq.com>
2026-01-25 13:01:48 +0800

    feat: update validate_json.py

--- scripts/validate_json.py
+++ scripts/validate_json.py
@@ -1,9 +1,11 @@
 import sys
+import traceback
 
 from rule import *
 
 try:
     importFromOS33("../module/immerse_rules.json").toData("33")
-    sys.exit(0)
 except:
+    traceback.print_exc()
     sys.exit(1)
+sys.exit(0)
