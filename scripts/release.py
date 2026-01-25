82c8abb9bd0f3fab3b61bbb3b92df784c030eb7d
Ianzb <93322252@qq.com>
2026-01-25 13:03:03 +0800

    feat: withdraw path change

--- scripts/release.py
+++ scripts/release.py
@@ -1,5 +1,5 @@
 from rule import *
 
-save_file("../module/immerse_rules.json", importFromOS33("../module/immerse_rules.json").toData("33"))
+save_file("module/immerse_rules.json", importFromOS33("module/immerse_rules.json").toData("33"))
 # save_file("module/immerse_rules_30.json", importFromOS33("module/immerse_rules.json").toData("30"))
-save_file("../module/immerse_rules.xml", importFromOS33("../module/immerse_rules.json").toData("22"))
+save_file("module/immerse_rules.xml", importFromOS33("module/immerse_rules.json").toData("22"))
--- scripts/validate_json.py
+++ scripts/validate_json.py
@@ -4,7 +4,7 @@ import traceback
 from rule import *
 
 try:
-    importFromOS33("../module/immerse_rules.json").toData("33")
+    importFromOS33("module/immerse_rules.json").toData("33")
 except:
     traceback.print_exc()
     sys.exit(1)
